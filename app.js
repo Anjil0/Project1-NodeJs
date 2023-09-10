64f091dacd52f8fddea0beb83328ddb70f1a6393
Anjil0 <angilneupane222@gmail.com>
2023-09-10 16:06:02 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    // res.send("WELCOME TO THE HOMEPAGE")
+    res.render('Blog')
+})
+
+app.get('/createBlog', (req, res) => {
+    // res.send("WELCOME TO Create Blog")
+    res.render('createBlog')
+
+})
+
+app.post('/createBlog', (req, res) => {
+    console.log(req.body)
+})
+
+app.listen(3000, () => {
+  console.log("Project Has Started!");
+});
