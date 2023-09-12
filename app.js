8a615bd950a1961701162e6a0d8e79ea9c37591a
Anjil0 <angilneupane222@gmail.com>
2023-09-12 12:43:20 +0545

    CRUD PAGE FINISHED

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// to get data from database and render it in homepage
 app.get("/", async (req, res) => {
   // to get data from database
   const allblogs = await blogs.findAll();
@@ -19,11 +20,13 @@ app.get("/", async (req, res) => {
   res.render("Blog", { allblogs: allblogs });
 });
 
+// this is for uiiii render
 app.get("/createBlog", (req, res) => {
   // res.send("WELCOME TO Create Blog")
   res.render("createBlog");
 });
 
+// this is for inserting data in database
 app.post("/createBlog", async (req, res) => {
   //Mathi bata user ley deko input constant ma haleko
   // const title = req.body.title;
@@ -48,12 +51,39 @@ app.get("/blog/:id", async (req, res) => {
   res.render("soloBlog", { singleBlog: singleBlog });
 });
 
+// to delete blog
 app.get("/delete/:id", async (req, res) => {
   const id = req.params.id;
   await blogs.destroy({ where: { id: id } });
   res.redirect("/");
 });
 
+// Edit blogs
+app.get("/edit/:id", async (req, res) => {
+  const id = req.params.id;
+  //finding blog to go in prefill edit mode
+  const blog = await blogs.findAll({ where: { id: id } });
+  res.render("editBlog",{ blog: blog });
+});
+
+
+//this is for updating data 
+app.post("/editBlog/:id", async (req, res) => {
+  const id = req.params.id;
+  const { title, subTitle, description } = req.body;
+  await blogs.update(
+    {
+      title: title,
+      subTitle: subTitle,
+      description: description,
+    },
+    { where: { id: id } }
+  );
+  res.redirect("/blog/"+id);
+  
+});
+
+// this is to run the port
 app.listen(3000, () => {
   console.log("Project Has Started!");
 });
